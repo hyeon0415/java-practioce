7dd2d27ee71c0e49a31a0d91668447deea7e902f
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-09-02 14:50:13 +0900

    2023. 09. 02
    class(1)

---
+++ src/sec04/chap07/Ex01.java
@@ -0,0 +1,10 @@
+package sec04.chap07;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        //  IDE가 최상단에 import java.util.Scanner 자동 작성
+        Scanner sc = new Scanner(System.in);
+    }
+}
